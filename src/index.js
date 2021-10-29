4ba12b9acfd47cb19c7e4ba5d0b08b15bef99d6c
Lu Khei <kheiboardwarrior@gmail.com>
2021-10-29 18:41:58 +0800

    convert app to pwa

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { configureAppStore } from './app/store';
 import App from './components/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
+import * as serviceWorker from './serviceWorker';
 
 const store = configureAppStore()
 
@@ -16,6 +16,11 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
 
 if (module.hot) { module.hot.accept(App); }
 // If you want to start measuring performance in your app, pass a function
